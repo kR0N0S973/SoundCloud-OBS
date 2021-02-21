c694667ccbf3133d0fee4aa8bb1fd9da17d8d405
Kevin Wiederstein <kevin.wiede@outlook.it>
2021-02-21 09:35:40 +0100

    Initial commit

---
+++ SoundCloud OBS/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace SoundCloud_OBS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.panel2);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ SoundCloud OBS/Form1.cs
@@ -0,0 +1,193 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundCloud_OBS
+{
+    public partial class Form1 : Form
+    {
+        string html = "";
+        string webserver_path = Application.StartupPath + @"\Widget";
+        CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        string localWebAdress = @"http://127.0.0.1:12345/index.html";
+        ChromiumWebBrowser widgetPreview;
+        SimpleHTTPServer myServer;
+        string song_author_class = "//a[@class='playbackSoundBadge__titleLink sc-truncate']";
+        string song_name_class = "//a[@class='playbackSoundBadge__lightLink sc-link-light sc-truncate']";
+        string song_image_class = "//span[@class='sc-artwork sc-artwork-placeholder-5  image__full g-opacity-transition']";
+
+        public Form1()
+        {
+            Cef.Initialize(new CefSettings { RemoteDebuggingPort = 8088, CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF", PersistSessionCookies = true });
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Directory.CreateDirectory(webserver_path);
+            ResourceManager rm = new ResourceManager("items", Assembly.GetExecutingAssembly());
+
+            string str = Ressources.ResourceManager.GetString("index");
+            if(str != string.Empty) 
+            {
+                //Application.StartupPath
+                string indexfile = webserver_path + @"\index.html";
+                if (!File.Exists(indexfile)) 
+                {
+                    StreamWriter wr = new StreamWriter(indexfile, false, System.Text.Encoding.UTF8);
+                    wr.Write(str);
+                    wr.Close();
+                }
+            }
+
+
+            // start widget server
+            myServer = new SimpleHTTPServer(webserver_path, 12345);
+            linkLabel1.Text = localWebAdress;
+            //linkLabel1.
+
+            chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser("http://soundcloud.com");
+            widgetPreview = new CefSharp.WinForms.ChromiumWebBrowser(localWebAdress);
+            panel2.Controls.Add(widgetPreview);
+            panel1.Controls.Add(chromiumWebBrowser1);
+            chromiumWebBrowser1.FrameLoadEnd += eoOnSiteLoaded;
+            
+        }
+
+        private void eoOnSiteLoaded(object sender, EventArgs e)
+        {
+            timer1.Interval = 2000;
+            timer1.Enabled = true;
+        }
+
+
+        private async Task<string> GetHtml() 
+        {
+            return await chromiumWebBrowser1.GetBrowser().MainFrame.GetSourceAsync();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if(!Directory.Exists(Application.StartupPath + @"\\Widget\\")) 
+            {
+                Directory.CreateDirectory(Application.StartupPath + @"\\Widget\\");
+            }
+
+            string f = webserver_path + @"\currentSource.txt";
+            string f2 = webserver_path + @"\currentSong.txt";
+            chromiumWebBrowser1.GetSourceAsync().ContinueWith(taskHtml =>
+            {
+                html = taskHtml.Result;
+              
+                if (File.Exists(f)) 
+                {
+                    File.Delete(f);
+                }
+
+                StreamWriter wr = new StreamWriter(f, false, System.Text.Encoding.UTF8);
+                wr.Write(html);
+                wr.Close();
+            });
+
+
+            try 
+            {
+            using (var sr = new StreamReader(f))
+            {
+                string test = sr.ReadToEnd();
+
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(test);
+
+                string text = "";
+
+                
+
+
+                HtmlAgilityPack.HtmlNode ts = doc.DocumentNode.SelectSingleNode(song_author_class);
+                if (ts != null)
+                {
+                    text += ts.Attributes["title"].Value + ";";
+                }
+
+                //
+
+                HtmlAgilityPack.HtmlNode ts2 = doc.DocumentNode.SelectSingleNode(song_name_class);
+                if (ts2 != null)
+                {
+                    text += ts2.Attributes["title"].Value + ";";
+                }
+
+                HtmlAgilityPack.HtmlNode ts3 = doc.DocumentNode.SelectSingleNode(song_image_class);
+                if (ts3 != null)
+                {
+                    string img = ts3.Attributes["style"].Value;
+
+                    int pFrom = img.IndexOf("url(") + "key : ".Length;
+                    int pTo = img.LastIndexOf(")");
+
+                    string result = img.Substring(pFrom, pTo - pFrom).Replace('"', ' ').Replace("&quot;", "");
+
+                    text += result + "\n";
+                }
+
+                if (File.Exists(f2))
+                {
+                    File.Delete(f2);
+                }
+
+                StreamWriter wr = new StreamWriter(f2, false, System.Text.Encoding.UTF8);
+                wr.Write(text);
+                wr.Close();
+                }
+            }catch(Exception ex) 
+            {
+            
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //chromiumWebBrowser1.Load("http://soundcloud.com");
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void chromiumWebBrowser2_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            myServer.Stop();
+            myServer = new SimpleHTTPServer(webserver_path, 12345);
+
+            widgetPreview.Reload(true);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel1.Text);
+        }
+    }
+}
